97b6c79a309da30659934c88abd2b5293dd20da5
Mathias95 <mathiasrjacobsen@gmail.com>
2018-12-21 19:53:44 +0100

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Starter-Kit
+
+ This is a small starterkit setup web developers. The goal was to create a small working environment for frontenders, and to save you time for setting it up yourself :)
+ 
+ **Steps**
+---------
+
+**Download or clone the repository**
+	You can download or clone the repo in the right corner where there is a green button (Clone or download).
+  
+ **Installing Node**
+  Remember to download and install Node.js in order to use NPM
+	https://nodejs.org/en/
+  
+ **Installing the node packages** 
+ Inside your project open and run the following commands in the terminal or cmd
+```bash
+    npm install
+    npm install gulp@4.0.0 -g
+```
+for more information about gulp: https://gulpjs.com/
+    
+ **Update the node packages** 
+To get the lates updates run the following command inside your project using the terminal or cmd
+```bash
+  npm update
+```
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp                = require('gulp'),
+    sass                = require('gulp-sass'),
+    cleanCSS            = require('gulp-clean-css'),
+    autoprefixer        = require('gulp-autoprefixer'),
+    rename              = require('gulp-rename'),
+    uglify              = require('gulp-uglify'),
+    concat              = require('gulp-concat'),
+    plumber             = require('gulp-plumber'),
+    clean               = require('gulp-clean'),
+    sourcemaps          = require('gulp-sourcemaps'),
+    htmlmin             = require('gulp-html-minifier'),
+    imagemin            = require('gulp-imagemin'),
+    browserSync         = require('browser-sync');
+
+var src                 = './src/',
+    dist                = './dist/';
+
+//HTML MINIFY
+gulp.task('html', function(){
+    gulp.src(dist + '*.html',{force: true})
+        .pipe(clean());
+    gulp.src(src + '*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(dist))
+        .pipe(browserSync.stream());
+});
+
+//SASS MINIFY
+gulp.task('styles', function(){
+    return gulp.src(src + 'assets/sass/**/*.scss')
+        .pipe(sourcemaps.init())
+            .pipe(plumber())
+            .pipe(sass()
+                .on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(rename({ basename: 'style'}))
+            .pipe(cleanCSS())
+            .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dist + 'assets/css'))
+        .pipe(browserSync.stream());
+});
+
+//Javascript MINIFY
+gulp.task('js', function(){
+    return gulp.src(src + 'assets/js/*.js')
+        .pipe(sourcemaps.init())
+            .pipe(plumber())
+            .pipe(concat('app.js'))
+            .pipe(uglify())
+            .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dist + 'assets/js'))
+        .pipe(browserSync.stream());
+});
+
+//Image compress
+gulp.task('images', function(){
+    return gulp.src(src + 'assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(dist + 'assets/images'));
+});
+
+//WATCH
+gulp.task('default', function(){
+
+    browserSync.init({
+        server: './dist'
+    });
+
+    gulp.watch([src + '*.html'], gulp.series('html'));
+    gulp.watch([src + 'assets/sass/**/*.scss'], gulp.series('styles'));
+    gulp.watch([src + 'assets/js/*.js'], gulp.series('js'));
+    gulp.watch([src + 'assets/images/*'], gulp.series('images'));
+});
